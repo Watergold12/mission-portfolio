66293ddcdcb2d3cdf9889cda65e65bbbf32379c5
WaterGold12 <techieman7@gmail.com>
2026-06-22 13:25:35 +0530

    feat:npx wrangler setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
   base: "/mission-control-hq/",
 
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cloudflare()],
 
   resolve: {
     alias: {
